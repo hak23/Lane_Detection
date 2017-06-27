da23aa67a36c76e81392493f2273f184338fafe3
hak23 <vigshru23@gmail.com>
2017-06-27 22:57:27 +0530

    Comments Enhanced. Minor cleanups

--- lane_detect.py
+++ lane_detect.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2
 
 VIDEO_INPUT = 'solidWhiteRight.mp4'
-# VIDEO_INPUT = 'challenge.mp4'
+VIDEO_INPUT = 'challenge.mp4'
+
 RHO_ACCURACY    = 1
 THETA_ACCURACY  = np.pi/90 
 MIN_VOTES       = 20
@@ -11,17 +12,25 @@ MAX_LINE_GAP    = 5
 
 FRAME_DELAY = 25
 
-FRAME_HEIGHT = 0
-FRAMR_WIDHT  = 0
-
 YELLOW_LOW  = np.asarray([20, 100, 100]) 
 YELLOW_HIGH = np.asarray([30, 255, 255])
 WHITE_LOW   = np.asarray([0, 0, 230])
 WHITE_HIGH  = np.asarray([255, 80, 255])
 
+FRAME_HEIGHT = 0
+FRAMR_WIDHT  = 0
+
 
 def get_slope(x1, y1, x2, y2):
+    '''
+    Get slope of line
     
+    :param x1, y1, x2, y2: 4 scalars with (x1,y1) and (x2,y2) as points
+
+    :ret dy/dx : slope of the line represented by thr two points
+    :ret None: In case there is a divide by zero
+    '''
+
     if x2 != x1:
         dy = float(y2) - float(y1)
         dx = float(x2) - float(x1)
@@ -33,7 +42,15 @@ def get_slope(x1, y1, x2, y2):
 
 
 def get_line_props(line):
-    '''define input here'''
+    '''
+    Get the slope and intercept of the line defined by collinear vector and a point on the line
+
+    :param line: A list of four elements [x1,y1,x2,y2] where (x1,y1) is a vector colliniear to the given line
+                 and (x2,y2) is a point on the line. Basically output of cv2.fitLine
+
+    :ret m,c: slope and intercept of the line passing through (x1,y1) and (x2,y2)
+    '''
+
     m = (float(line[1])/line[0])
     c = float(line[3]) - (m * line[2])
 
@@ -41,6 +58,16 @@ def get_line_props(line):
 
 
 def color_threshold(frame_in, frame_hsv):
+    '''
+    Color thresholding on the input image. Black out the pixels which do not fall in the 
+    range of yellow and white thresholds
+
+    :param frame_in: Input frame
+    :param frame_hsv: Input frame in hsv space
+
+    :ret threshold: The thresholded frame 
+    '''
+
     yellow_mask = cv2.inRange(frame_hsv, YELLOW_LOW, YELLOW_HIGH)
     white_mask  = cv2.inRange(frame_hsv, WHITE_LOW, WHITE_HIGH)
     mask = cv2.bitwise_or(yellow_mask, white_mask)
@@ -50,7 +77,17 @@ def color_threshold(frame_in, frame_hsv):
     return threshold
 
 
-def preprocess_image(frame_in): 
+def preprocess_image(frame_in):
+    '''
+    Do some preprocessing on the input frame. Discard not needed colors (Lane lines are mostly white or yellow).
+    Get the edge image and carve out unnecessary region in the frame. Note that the carve out region depends on
+    the placement of camera.
+
+    :param frame_in: Input frame
+
+    :ret frame_edge: preprocessed frame
+    '''
+
     frame_h = frame_in.shape[0] 
     frame_w = frame_in.shape[1]
     frame_hsv = cv2.cvtColor(frame_in, cv2.COLOR_BGR2HSV)
@@ -66,6 +103,14 @@ def preprocess_image(frame_in):
 
 
 def get_line_list(frame_in):
+    '''
+    Use Hough transform to get Lines in terms of point coordinates
+    
+    :param frame_in: Input frame
+
+    :ret lines_list: A list of Lines (in terms of  point coordinates)
+    '''
+
     lines_list = cv2.HoughLinesP(frame_in, RHO_ACCURACY, THETA_ACCURACY, MIN_VOTES, MIN_LINE_LENGTH, MAX_LINE_GAP)
     # omit the first dimension
     lines_list = np.squeeze(lines_list, 1)
@@ -74,12 +119,20 @@ def get_line_list(frame_in):
 
 
 def process_lines(lines_list):
+    '''
+    Distinguish points into rigth lane, left lane and neither(ignore)
+
+    :param lines_list: A list of lines represented by two points. Basically the output of cv2.HoughLinesP
+
+    :ret right_lane, left_lane: Two lists. One containing the points forming the right lane and other the left lane
+    '''
+    
     right_lane = []
     left_lane = []
 
     for lines in lines_list:
         slope = get_slope(lines[0], lines[1], lines[2], lines[3])
-        # Lane lines are not horizontal
+        # These lines would not be on either lanes.
         if -0.2 < slope < 0.2 :
             continue
         
@@ -117,9 +170,9 @@ if __name__ == '__main__':
             # Get list of lines with hough transform on frame 
             lines_list = get_line_list(frame_)
     
-            # Process lines
+            # check if lines_list is not empty
             if np.any(np.equal(lines_list, None)):
-                print "skipping"
+                print "skipping frame"
                 continue
             else:
                 right_lane, left_lane = process_lines(lines_list)
